4aadd5c424fccc61b331d01f2ea6b8ffdeadd264
edgarasratas <73912309+edgarasratas@users.noreply.github.com>
2021-05-18 18:18:20 +0300

    Add files via upload

---
+++ unitTesting.cpp
@@ -0,0 +1,23 @@
+#include "Student.h"
+//#include "unitTesting.h"
+
+//TEST(StudentTest, examGradeExists)
+//{
+//    Student examGrade;
+//    EXPECT_EQ(0, examGrade.getExamGrade());
+//    EXPECT_TRUE(1 == 1);
+//}
+//
+//TEST(StudentTest, numOfGradesExists)
+//{
+//    Student numOfGrades;
+//    EXPECT_EQ(0, numOfGrades.getNumOfGrades());
+//    EXPECT_TRUE(1 == 1);
+//}
+//
+//TEST(HumanTest, nameExists)
+//{
+//    Student name;
+//    EXPECT_EQ("", name.getName());
+//    EXPECT_TRUE(1 == 1);
+//}
